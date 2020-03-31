d1c0f168ef6a051f144ad5f1c5eee209c1216cba
KamillaKlavsen <kamillaklavsen@outlook.dk>
2020-03-31 09:57:25 +0200

    JS til WP
    Forsøgt, men ikke lykkedes

--- assets/js/script.js
+++ assets/js/script.js
@@ -56,4 +56,74 @@ window.onclick = function(event) {
   }
 }
 
-//FORTSÆTTES TEORETISK TIL DE SIDSTE 25 BANDS 
+//FORTSÆTTES TEORETISK TIL DE SIDSTE 25 BANDS
+
+//WORDPRESS - BILLEDER TIL MODAL
+//DETTE ER EN PRØVE - VIRKER IKKE 
+
+
+// *** BEGIN global variables ***
+// these variables' values depend on the WordPress instance - independent from the (web)app's execution
+
+// const apiUrl = 'http://kamkla1.dreamhosters.com/wp-json/wp/v2/';
+// const apiKey = '4VThLkb5UooxJsBKmA1lHaNdyTHEMiI6';
+// const postOptionsId = 20;
+
+
+// these variables hold information depending on the app's execution
+
+
+// let appOptions; // app options object from WP 
+
+
+// these variables refer to DOM elements; 'static' containers - invariable between executions of the app
+
+
+// const bandImage = document.querySelector('#bandImage');
+
+
+// *** END global variables ***
+
+// *** BEGIN execution ***
+// initPage();
+// *** END execution ***
+
+
+// errorMessage(msg) - displays error message
+
+// function errorMessage(msg) {
+//     console.log(msg);
+// }
+
+
+// ****************
+// WE START HERE vv
+// initPage() - initialises the page
+
+// function initPage() {
+
+//     // get hero image from WP
+//     getOptionsFromWP();
+// }
+
+// function getOptionsFromWP() {
+//     const xhttp = new XMLHttpRequest();
+//     xhttp.onreadystatechange = function () {
+//         if (this.readyState == 4 && this.status == 200) {
+//             // all good
+//             try {
+//                 appOptions = JSON.parse(this.responseText);
+//                 bandImage.src = appOptions.acf.band_image;
+//             } catch (error) {
+//                 errorMessage(`Error parsing JSON: ${error}`);
+//             }
+//         }
+//         if (this.readyState == 4 && this.status > 400) {
+//             errorMessage(`An error has occured while getting the data. Please try again later!`);
+//         }
+//     }
+//     xhttp.open('GET', `${apiUrl}posts/${postOptionsId}`, true);
+//     console.log(this);
+//     xhttp.setRequestHeader('Authorization', `Bearer ${apiKey}`);
+//     xhttp.send();
+// }
